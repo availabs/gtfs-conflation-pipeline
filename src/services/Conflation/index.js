9fa5fcc241c7c56872fead4b710aef1660f97603
Paul Tomchik <pauljtomchik@gmail.com>
2020-09-10 11:09:12 -0400

    Forgotten changes for commit 3527263b004e43c856cb85fbcb70b79e787ad19b

--- src/services/Conflation/index.js
+++ src/services/Conflation/index.js
@@ -1,7 +1,9 @@
 const { matchSegmentedShapeFeatures } = require("./SharedStreetsMatcher");
 const chooseShstMatchesForShape = require("./chooseShstMatchesForShape");
+const scoreChosenPaths = require("./scoreChosenPaths");
 
 module.exports = {
   matchSegmentedShapeFeatures,
   chooseShstMatchesForShape,
+  scoreChosenPaths,
 };
